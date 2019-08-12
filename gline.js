f4f275634897d9c8ccfb8bb31f17e778604b20f8
Sabha Kandula <sabha.kandula@gmail.com>
2019-08-12 11:07:29 -0400

    Initial Commit

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','indiana.edu','cam.ac.uk','mac.com','uct.ac.za','berkeley.edu','unicon.net','ucdavis.edu','gmail.com','stanford.edu'],
+['2005',57,12,7,12,14,12,6,11,10,4],
+['2006',873,405,403,350,294,276,279,216,213,162]
+];
